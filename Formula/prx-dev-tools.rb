137cf6d07b390bd0a35d844a53fbed079c3aeb5e
Chris Kalafarski <chris@farski.com>
2021-11-16 10:39:30 -0500

    Update formula

--- Formula/prx-dev-tools.rb
+++ Formula/prx-dev-tools.rb
@@ -1,15 +1,15 @@
 class PrxDevTools < Formula
   desc "PRX developer tools"
   homepage "https://github.com/PRX/homebrew-dev-tools"
-  url "https://github.com/PRX/homebrew-dev-tools/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "6aa1850089c5de68003e06c542798e6091c71dbe6b79138587528904f47e4fe0"
+  url "https://github.com/PRX/homebrew-dev-tools/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "839e32f6b37f4f1a5fc78e0a5ef038ba9666adbc0f5e6b40c69c4cd1fe6b8c3d"
   license ""
 
   def install
-    bin.install "bin/awssh-beta"
+    bin.install "bin/awssh"
   end
 
   test do
-    assert_match "awssh 0.0.1", shell_output("#{bin}/awssh-beta -v", 2)
+    assert_match "Usage", shell_output("#{bin}/awssh -v", 2)
   end
 end
