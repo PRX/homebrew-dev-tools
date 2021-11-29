d79810b282ee5ebd885527197ba019d1eb9f9086
Chris Kalafarski <chris@farski.com>
2021-11-29 09:27:35 -0500

    Update formula

--- Formula/prx-dev-tools.rb
+++ Formula/prx-dev-tools.rb
@@ -1,8 +1,8 @@
 class PrxDevTools < Formula
   desc "PRX developer tools"
   homepage "https://github.com/PRX/homebrew-dev-tools"
-  url "https://github.com/PRX/homebrew-dev-tools/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "839e32f6b37f4f1a5fc78e0a5ef038ba9666adbc0f5e6b40c69c4cd1fe6b8c3d"
+  url "https://github.com/PRX/homebrew-dev-tools/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "589d8990699562c0574ff79ecce97f52fa4f9ccddba8e10265609233fa650621"
   license ""
 
   def install
