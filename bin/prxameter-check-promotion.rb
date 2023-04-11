675817af7fc7a654389eb0335384025f924b3514
Chris Kalafarski <chris@farski.com>
2023-04-11 12:43:37 -0400

    Lint prxameter-check-promotion

--- bin/prxameter-check-promotion.rb
+++ bin/prxameter-check-promotion.rb
@@ -21,10 +21,10 @@ gemfile do
 end
 
 OPTS = Slop.parse do |o|
-  o.string '--profile', 'AWS profile', default: 'prx-legacy'
-  o.string '--region', 'Region (e.g., "us-east-1")'
-  o.bool '--hide-matches', 'Hide matching values'
-  o.on '-h', '--help' do
+  o.string "--profile", "AWS profile", default: "prx-legacy"
+  o.string "--region", 'Region (e.g., "us-east-1")'
+  o.bool "--hide-matches", "Hide matching values"
+  o.on "-h", "--help" do
     puts o
     exit
   end
@@ -38,12 +38,12 @@ client = Aws::SSM::Client.new(region: region, credentials: PrxRubyAwsCreds.clien
 stag_parameters = []
 prod_parameters = []
 
-client.get_parameters_by_path({ path: '/prx/stag/Spire', recursive: true, with_decryption: true }).each do |resp|
+client.get_parameters_by_path({path: "/prx/stag/Spire", recursive: true, with_decryption: true}).each do |resp|
   parameters = resp[:parameters]
   stag_parameters.concat(parameters)
 end
 
-client.get_parameters_by_path({ path: '/prx/prod/Spire', recursive: true, with_decryption: true }).each do |resp|
+client.get_parameters_by_path({path: "/prx/prod/Spire", recursive: true, with_decryption: true}).each do |resp|
   parameters = resp[:parameters]
   prod_parameters.concat(parameters)
 end
@@ -51,26 +51,25 @@ end
 all_normalized_parameter_names = []
 [stag_parameters, prod_parameters].each do |list|
   list.each do |parameter|
-    all_normalized_parameter_names.push(parameter.name.gsub('/prx/stag/', '/prx/*/').gsub('/prx/prod/', '/prx/*/'))
+    all_normalized_parameter_names.push(parameter.name.gsub("/prx/stag/", "/prx/*/").gsub("/prx/prod/", "/prx/*/"))
   end
 end
 
-headings = ['Parameter Name', '🔄', 'Staging', 'Production']
+headings = ["Parameter Name", "🔄", "Staging", "Production"]
 rows = []
 
 all_normalized_parameter_names.each do |name|
-  stag_parameter = stag_parameters.find {|p| name == p.name.gsub('/prx/stag/', '/prx/*/')}
-  prod_parameter = prod_parameters.find {|p| name == p.name.gsub('/prx/prod/', '/prx/*/')}
+  stag_parameter = stag_parameters.find { |p| name == p.name.gsub("/prx/stag/", "/prx/*/") }
+  prod_parameter = prod_parameters.find { |p| name == p.name.gsub("/prx/prod/", "/prx/*/") }
 
-  stag_value = stag_parameter&.value || '—'
-  prod_value = prod_parameter&.value || '—'
+  stag_value = stag_parameter&.value || "—"
+  prod_value = prod_parameter&.value || "—"
 
   next if stag_value == prod_value && hide_matches
 
   if name.include? "/pkg/"
-    rows << [name, stag_value == prod_value ? '✅' : '❌', stag_value[0, 50].split("\n")[0], prod_value[0, 50].split("\n")[0]]
+    rows << [name, (stag_value == prod_value) ? "✅" : "❌", stag_value[0, 50].split("\n")[0], prod_value[0, 50].split("\n")[0]]
   end
-
 end
 
 puts Terminal::Table.new headings: headings, rows: rows
