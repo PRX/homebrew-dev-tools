50f7c157569efad0b5bdd036a0ec5dfd17d86e3f
Chris Kalafarski <chris@farski.com>
2023-10-17 10:01:48 -0400

    Fix installation and standardrb config

--- Formula/prx-dev-tools.rb
+++ Formula/prx-dev-tools.rb
@@ -7,18 +7,18 @@ class PrxDevTools < Formula
 
   def install
     bin.install "bin/awssh"
-    bin.install "src/awsso.rb" => "awsso"
+    bin.install "bin/awsso.rb" => "awsso"
     bin.install "bin/awstunnel"
-    bin.install "src/prx-cfn-tree.rb" => "prx-cfn-tree"
-    bin.install "src/prx-empty-log-groups.rb" => "prx-empty-log-groups"
-    bin.install "src/prx-github-repo-audit.rb" => "prx-github-repo-audit"
-    bin.install "src/prx-immortal-log-groups.rb" => "prx-immortal-log-groups"
-    bin.install "src/prx-reservations.rb" => "prx-reservations"
-    bin.install "src/prxameter-audit-usage.rb" => "prxameter-audit-usage"
-    bin.install "src/prxameter-check-promotion.rb" => "prxameter-check-promotion"
-    bin.install "src/prxameter-check-sync.rb" => "prxameter-check-sync"
-    bin.install "src/prxameter-list.rb" => "prxameter-list"
-    bin.install "src/prxameter-region-sync.rb" => "prxameter-region-sync"
+    bin.install "bin/prx-cfn-tree.rb" => "prx-cfn-tree"
+    bin.install "bin/prx-empty-log-groups.rb" => "prx-empty-log-groups"
+    bin.install "bin/prx-github-repo-audit.rb" => "prx-github-repo-audit"
+    bin.install "bin/prx-immortal-log-groups.rb" => "prx-immortal-log-groups"
+    bin.install "bin/prx-reservations.rb" => "prx-reservations"
+    bin.install "bin/prxameter-audit-usage.rb" => "prxameter-audit-usage"
+    bin.install "bin/prxameter-check-promotion.rb" => "prxameter-check-promotion"
+    bin.install "bin/prxameter-check-sync.rb" => "prxameter-check-sync"
+    bin.install "bin/prxameter-list.rb" => "prxameter-list"
+    bin.install "bin/prxameter-region-sync.rb" => "prxameter-region-sync"
   end
 
   test do
